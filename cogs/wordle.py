05c60dd67a21b8661d7da4af468b6f15cd4b38a0
jianlel <hoojianle12@gmail.com>
2025-03-29 21:14:22 +0800

    implemented daily word challenge

--- cogs/wordle.py
+++ cogs/wordle.py
@@ -3,10 +3,16 @@ import database.database as db
 
 import logging
 import random
+import datetime
 
 WORDS = []
 with open("words.txt", 'r') as file:
     WORDS = [line.strip() for line in file.readlines()]
+
+def get_daily_word():
+    today = datetime.date.today()
+    random.seed(today.toordinal())
+    return random.choice(WORDS)
 
 def check_guess(guess, target):
     result = ["⬜"] * len(guess)  # Initialize result as all gray
@@ -39,6 +45,11 @@ class Wordle(commands.Cog):
     # Command to start wordle game
     @commands.command()
     async def wordle(self, ctx):
+        # Check for active game
+        if ctx.author.id in self.players and not self.players[ctx.author.id]["game_over"]:
+            await ctx.send("You already have an active game. Finish it before starting a new one.")
+            return
+        
         # Start the wordle game
         self.players[ctx.author.id] = {
             "attempts": 6,
@@ -48,6 +59,28 @@ class Wordle(commands.Cog):
         await ctx.send("Welcome to Wordle! Guess a 5-letter word.")
         logging.info(f'{ctx.author} started a Wordle game')
         logging.info(f'Word is {self.players[ctx.author.id]["target_word"]}')
+
+    # Command to start Daily wordle game
+    @commands.command()
+    async def daily(self, ctx):
+        if await db.has_played_today(ctx.author.id):
+            await ctx.send("You have already completed today's Daily Challenge!")
+            return
+        
+        # Check for active game
+        if ctx.author.id in self.players and not self.players[ctx.author.id]["game_over"]:
+            await ctx.send("You already have an active game. Finish it before starting a new one.")
+            return
+
+        # Start the daily challenge
+        self.players[ctx.author.id] = {
+            "attempts": 6,
+            "target_word": get_daily_word(),
+            "game_over": False,
+            "daily": True
+        }
+        await ctx.send("Welcome to the Daily Wordle Challenge! Guess today's 5-letter word.")
+        logging.info(f'{ctx.author} started a Daily Wordle challenge')
 
     # Command to make a guess
     @commands.command()
@@ -72,25 +105,41 @@ class Wordle(commands.Cog):
         feedback = check_guess(guess, target_word)
         await ctx.send(feedback)
 
-        # If guess is correct, end the game
+        # If guess is correct, end the game!
         if guess == target_word:
-            await db.win(ctx.author.id)
-            #db.increment_streak(ctx.author.id)
-            streak = await db.get_streak(ctx.author.id)
-            await ctx.send(f"You won! Current streak = {streak}")
-            player_data["game_over"] = True
-            logging.info(f'{ctx.author} won the Wordle game! The word was {target_word}. Current streak" {streak}')
-            return
+            if player_data.get("daily"):
+                await db.mark_daily_completed(ctx.author.id)
+                await db.increment_daily_streak(ctx.author.id)
+                daily_streak = await db.get_daily_streak(ctx.author.id)
+                await ctx.send(f"You won today's Daily Challenge! The word was {target_word}. Current Daily streak = {daily_streak}")
+                player_data["game_over"] = True
+                logging.info(f'{ctx.author} won the Daily Wordle game! The word was {target_word}. Current daily streak: {daily_streak}')
+                return
+            else:
+                await db.win(ctx.author.id)
+                #db.increment_streak(ctx.author.id)
+                streak = await db.get_streak(ctx.author.id)
+                await ctx.send(f"You won! Current streak = {streak}")
+                player_data["game_over"] = True
+                logging.info(f'{ctx.author} won the Wordle game! The word was {target_word}. Current streak: {streak}')
+                return
         
         # If guess is incorrect, decrease attempts
         attempts -= 1
         self.players[ctx.author.id]["attempts"] = attempts
         if attempts == 0:
-            await ctx.send(f"Game over! The word was: {target_word}")
-            await db.reset_streak(ctx.author.id)
-            await db.lose(ctx.author.id)
-            player_data["game_over"] = True
-            logging.info(f'{ctx.author} loss the Wordle game! The word was {target_word}.')
+            if player_data.get("daily"):
+                await ctx.send(f"Game over! The daily word was: {target_word}")
+                await db.reset_daily_streak(ctx.author.id)
+                player_data["game_over"] = True
+                logging.info(f'{ctx.author} loss the Daily Wordle game! The word was {target_word}.')
+
+            else:
+                await ctx.send(f"Game over! The word was: {target_word}")
+                await db.reset_streak(ctx.author.id)
+                await db.lose(ctx.author.id)
+                player_data["game_over"] = True
+                logging.info(f'{ctx.author} loss the Wordle game! The word was {target_word}.')
 
         if attempts > 0:
             await ctx.send(f"You have {attempts} attempts left.")
@@ -109,15 +158,20 @@ class Wordle(commands.Cog):
     @commands.command()
     async def streak(self, ctx):
         streak = await db.get_streak(ctx.author.id)
-        await ctx.send(f"Your current streak is {streak}")
+        daily_streak = await db.get_daily_streak(ctx.author.id)
+        await ctx.send(f"Your game streak: {streak}, Your daily streak: {daily_streak}")
     
     # Command to see user stats
     @commands.command()
     async def stats(self, ctx):
         wins = await db.get_wins(ctx.author.id)
         losses = await db.get_losses(ctx.author.id)
-        await ctx.send(f"Wins: {wins}")
-        await ctx.send(f"Losses: {losses}")
+        if wins != 0:
+            win_percent = wins / (wins + losses) * 100
+            await ctx.send(f"Wins: {wins}, Losses: {losses}, Percentage: {round(win_percent, 1)}%")
+        else:
+            await ctx.send(f"Wins: {wins}, Losses: {losses}")
 
 async def setup(bot):
+    await db.init_db()
     await bot.add_cog(Wordle(bot))
--- database/database.py
+++ database/database.py
@@ -1,4 +1,5 @@
 import aiosqlite
+import datetime
 
 DB_PATH = "database/wordle.db"
 
@@ -7,9 +8,17 @@ async def init_db():
         await db.execute('''
             CREATE TABLE IF NOT EXISTS streaks (
                 user_id INTEGER PRIMARY KEY,
+                daily_streak Integer, 
                 streak INTEGER,
                 wins INTEGER,
                 losses INTEGER
+            )
+        ''')
+        await db.execute('''
+            CREATE TABLE IF NOT EXISTS daily_log (
+                user_id INTEGER,
+                date TEXT,
+                PRIMARY KEY (user_id, date)
             )
         ''')
         await db.commit()
@@ -61,4 +70,37 @@ async def lose(user_id):
 async def reset_streak(user_id):
     async with aiosqlite.connect(DB_PATH) as db:
         await db.execute('UPDATE streaks SET streak = 0 WHERE user_id = ?', (user_id,))
+        await db.commit()
+
+async def get_daily_streak(user_id):
+    async with aiosqlite.connect(DB_PATH) as db:
+        async with db.execute('SELECT daily_streak FROM streaks WHERE user_id = ?', (user_id,)) as cursor:
+            row = await cursor.fetchone()
+            return row[0] if row else 0
+
+async def increment_daily_streak(user_id):
+    if not await user_exists(user_id):
+        async with aiosqlite.connect(DB_PATH) as db:
+            await db.execute('INSERT INTO streaks (user_id, streak, daily_streak, wins, losses) VALUES (?, ?, ?, ?, ?)', (user_id, 0, 1, 0, 0))
+            await db.commit()
+    else:
+        async with aiosqlite.connect(DB_PATH) as db:
+            await db.execute('UPDATE streaks SET daily_streak = daily_streak + 1 WHERE user_id = ?', (user_id,))
+            await db.commit()
+
+async def reset_daily_streak(user_id):
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute('UPDATE streaks SET daily_streak = 0 WHERE user_id = ?', (user_id,))
+        await db.commit()
+
+async def has_played_today(user_id):
+    today = str(datetime.date.today())
+    async with aiosqlite.connect(DB_PATH) as db:
+        async with db.execute('SELECT 1 FROM daily_log WHERE user_id = ? AND date = ?', (user_id, today)) as cursor:
+            return await cursor.fetchone() is not None
+
+async def mark_daily_completed(user_id):
+    today = str(datetime.date.today())
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute('INSERT OR IGNORE INTO daily_log (user_id, date) VALUES (?, ?)', (user_id, today))
         await db.commit()
